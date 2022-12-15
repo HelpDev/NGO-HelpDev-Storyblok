7dddae9b73bf0cd194a3aed280f6a2c26f86880f
Quique Fdez Guerra <quique@ckgrafico.com>
2022-12-15 12:30:47 +0200

    init header

--- components/Header.vue
+++ components/Header.vue
@@ -1,46 +1,95 @@
 <script setup>
-import { Column, Row } from '@papanasi/vue';
+import { Column, Container, Row } from '@papanasi/vue';
 const storyblokApi = useStoryblokApi();
+const { locale } = useI18n();
 
 const { data } = await storyblokApi.get('cdn/stories/config', {
   version: 'draft',
-  resolve_links: 'url'
+  resolve_links: 'url',
+  language: locale.value
 });
 
 const headerMenu = ref(data.story.content.header_menu);
 </script>
 
 <template>
-  <Row class="header">
-    <Column>
-      <NuxtLink to="/">
-        <img src="~/assets/images/logo_landscape_white.png" :alt="data.story.content.title" width="100" />
-      </NuxtLink>
-    </Column>
-    <Column>
-      <nav v-if="headerMenu">
-        <ul>
-          <li v-for="blok in headerMenu" :key="blok._uid">
-            <NuxtLink
-              :to="
-                blok.link.cached_url.includes('index')
-                  ? blok.link.cached_url.replace('index', '/')
-                  : blok.link.cached_url
-              "
-            >
-              {{ blok.link.story.name }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </Column>
-  </Row>
+  <Container :fluid="true" class="header">
+    <Row basic="column" s="row" class="header__container">
+      <Column :s="2" class="header__column">
+        <NuxtLink to="/">
+          <img
+            class="header__logo"
+            src="~/assets/images/logo_landscape_white.png"
+            :alt="data.story.content.title"
+            width="200"
+          />
+        </NuxtLink>
+      </Column>
+      <Column class="header__column">
+        <nav v-if="headerMenu" class="header__nav">
+          <ul class="header__menu">
+            <li v-for="blok in headerMenu" :key="blok._uid" class="header__link">
+              <NuxtLink
+                :to="
+                  blok.link.cached_url.includes('index')
+                    ? blok.link.cached_url.replace('index', '/')
+                    : blok.link.cached_url
+                "
+              >
+                {{ blok.title }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </Column>
+      <Column :s="2" class="header__column"> Lang </Column>
+    </Row>
+  </Container>
 </template>
 
 <style scoped>
 .header {
-  background-color: var(--color-primary-normal);
-  position: sticky;
-  top: 0;
+  position: absolute;
+  top: 1rem;
+  height: var(--header-height);
+
+  @media (--breakpoint-s) {
+    top: 0;
+  }
+
+  &__container {
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    max-width: var(--pa-grid-container-max-width);
+    width: 90vw;
+    height: var(--header-height);
+  }
+
+  &__logo {
+    max-width: 8rem;
+
+    @media (--breakpoint-s) {
+      max-width: 13rem;
+    }
+  }
+
+  &__column {
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__nav,
+  &__menu {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+  }
+
+  &__menu {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+  }
 }
 </style>
--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -6,13 +6,17 @@ defineProps({ blok: Object });
 
 <template>
   <div class="hero">
-    <Container class="hero__container" v-editable="blok">
+    <Container
+      v-editable="blok"
+      :fluid="true"
+      class="hero__container"
+      :style="{ 'background-image': `url(${blok.image.filename})` }"
+    >
       <Row basic="column" m="row">
-        <Column>
+        <Column class="hero__elements">
           <h1 class="hero__title">{{ blok.title }}</h1>
           <p class="hero__subtitle">{{ blok.subtitle }}</p>
         </Column>
-        <Column> <img :src="blok.image.filename" :alt="blok.title" /> </Column>
       </Row>
     </Container>
   </div>
@@ -20,17 +24,65 @@ defineProps({ blok: Object });
 
 <style scoped>
 .hero {
+  --hero-height: 90vh;
+
   background-color: var(--color-primary-darker);
   background-image: linear-gradient(180deg, var(--color-primary-normal) 10%, var(--color-primary-normal) 100%);
+  height: var(--hero-height);
+  overflow: hidden;
 
   &__container {
+    background-position: 175% top;
+    background-repeat: no-repeat;
+    background-size: contain;
     margin: 0 auto;
+    padding-top: var(--header-height);
+    height: var(--hero-height);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  &__elements {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    height: calc(var(--hero-height) - var(--header-height));
   }
 
   &__title {
+    color: var(--color-basic-brightest);
     font-family: var(--font-family-heading);
-    font-size: var(--font-size-xxxl);
-    font-weight: var(--font-weight-black);
+    font-size: var(--font-size-xxl);
+    font-weight: var(--font-weight-bold);
+    text-align: center;
+    max-width: 80%;
+
+    @media (--breakpoint-s) {
+      font-size: var(--font-size-xxxl);
+    }
+
+    @media (--breakpoint-m) {
+      max-width: 35rem;
+    }
+  }
+
+  &__subtitle {
+    color: var(--color-basic-brightest);
+    font-size: var(--font-size-m);
+    line-height: calc(var(--font-size-m) * 1.5);
+    text-align: center;
+    max-width: 80%;
+
+    @media (--breakpoint-s) {
+      font-size: var(--font-size-l);
+      line-height: calc(var(--font-size-l) * 1.5);
+    }
+
+    @media (--breakpoint-m) {
+      max-width: 40rem;
+    }
   }
 }
 </style>
