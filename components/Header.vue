a71aded882cc451c751db64fea1305fba703f22e
Quique Fdez Guerra <quique@ckgrafico.com>
2023-01-10 17:55:53 +0200

    add gtag

--- components/Header.vue
+++ components/Header.vue
@@ -19,7 +19,20 @@ useHead({
     lang: locale.value
   },
   title: store.title,
-  meta: [{ name: 'description', content: store.description }]
+  meta: [{ name: 'description', content: store.description }],
+  script: [
+    {
+      hid: 'gtm',
+      children: data.story.content.gtag
+        ? `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${data.story.content.gtag}');`
+        : '',
+      type: 'text/javascript'
+    }
+  ]
 });
 
 const menu = ref(store.menu);
